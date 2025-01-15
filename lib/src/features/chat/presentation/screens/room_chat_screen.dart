e6605f6595f5b936528d61ec5272b221c0cbd156
VŨ NGỌC HẢI HÀ <84841550+GonDraz@users.noreply.github.com>
2025-01-15 20:45:59 +0700

    room chat ui

--- lib/src/features/chat/presentation/screens/room_chat_screen.dart
+++ lib/src/features/chat/presentation/screens/room_chat_screen.dart
@@ -1,17 +1,14 @@
-import 'dart:convert';
 import 'dart:ui';
 
+import 'package:chatview/chatview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:hive_ce_flutter/adapters.dart';
-import 'package:hubtsocial_mobile/src/core/api/api_request.dart';
 import 'package:hubtsocial_mobile/src/core/extensions/context.dart';
-import 'package:hubtsocial_mobile/src/features/auth/domain/entities/user_token.dart';
-import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+import 'package:hubtsocial_mobile/src/core/logger/logger.dart';
+import 'package:hubtsocial_mobile/src/core/presentation/widget/url_image.dart';
 
-import '../../../../core/local_storage/local_storage_key.dart';
-import '../../../../core/presentation/widget/url_image.dart';
-import '../../../main_wrapper/ui/widgets/main_app_bar.dart';
+import 'data.dart';
+import 'theme.dart';
 
 class RoomChatScreen extends StatefulWidget {
   const RoomChatScreen({required this.id, required this.title, super.key});
@@ -23,113 +20,345 @@ class RoomChatScreen extends StatefulWidget {
 }
 
 class _RoomChatScreenState extends State<RoomChatScreen> {
-  final _pagingController = PagingController<int, String>(
-    firstPageKey: 1,
+  AppTheme theme = LightTheme();
+  bool isDarkTheme = false;
+  final _chatController = ChatController(
+    initialMessageList: Data.messageList,
+    scrollController: ScrollController(),
+    currentUser: ChatUser(
+      id: '1',
+      name: 'Flutter',
+      profilePhoto: Data.profileImage,
+    ),
+    otherUsers: [
+      ChatUser(
+        id: '2',
+        name: 'Simform',
+        profilePhoto: Data.profileImage,
+      ),
+      ChatUser(
+        id: '3',
+        name: 'Jhon',
+        profilePhoto: Data.profileImage,
+      ),
+      ChatUser(
+        id: '4',
+        name: 'Mike',
+        profilePhoto: Data.profileImage,
+      ),
+      ChatUser(
+        id: '5',
+        name: 'Rich',
+        profilePhoto: Data.profileImage,
+      ),
+    ],
   );
 
-  @override
-  void initState() {
-    _pagingController.addPageRequestListener((pageKey) {
-      _fetchPage(pageKey);
-    });
-    super.initState();
+  void _showHideTypingIndicator() {
+    _chatController.setTypingIndicator = !_chatController.showTypingIndicator;
   }
 
-  Future<void> _fetchPage(int pageKey) async {
-    try {
-      var tokenBox = Hive.box(LocalStorageKey.token);
-      if (tokenBox.isEmpty ||
-          !tokenBox.containsKey(LocalStorageKey.userToken)) {
-      } else if (tokenBox.isNotEmpty &&
-          tokenBox.containsKey(LocalStorageKey.userToken)) {
-        UserToken token = tokenBox.get(LocalStorageKey.userToken);
-
-        final response = await APIRequest.get(
-            url:
-                "https://hubt-social-develop.onrender.com/api/chat/user/get-rooms",
-            token: token.accessToken.toString());
-
-        if (response.statusCode == 200) {
-          final List newItems = json.decode(response.body);
-
-          List<String> items = [];
-
-          items.addAll(newItems.map<String>((item) {
-            final id = item['id'];
-            return 'Item $id';
-          }).toList());
-
-          if (items.isEmpty) {
-            _pagingController.error = "items isEmpty";
-          } else {
-            _pagingController.appendPage(items, pageKey + 1);
-          }
-        }
-      }
-    } catch (e) {
-      _pagingController.error = e;
-    }
-  }
-
-  @override
-  void dispose() {
-    _pagingController.dispose();
-    super.dispose();
+  void receiveMessage() async {
+    _chatController.addMessage(
+      Message(
+        id: DateTime.now().toString(),
+        message: 'I will schedule the meeting.',
+        createdAt: DateTime.now(),
+        sentBy: '2',
+      ),
+    );
+    await Future.delayed(const Duration(milliseconds: 500));
+    _chatController.addReplySuggestions([
+      const SuggestionItemData(text: 'Thanks.'),
+      const SuggestionItemData(text: 'Thank you very much.'),
+      const SuggestionItemData(text: 'Great.')
+    ]);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true,
-      extendBody: true,
-      body: NestedScrollView(
-        reverse: true,
-        headerSliverBuilder: (context, innerBoxIsScrolled) => [
-          SliverAppBar(
-            pinned: true,
-            toolbarHeight: 60.h,
-            titleSpacing: 0,
-            backgroundColor:
-                // Theme.of(context).colorScheme.primary.withAlpha(220),
-                Theme.of(context).colorScheme.primary,
-            floating: true,
-            snap: true,
-            // flexibleSpace: ClipRRect(
-            //   child: BackdropFilter(
-            //     filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-            //     blendMode: BlendMode.srcOver,
-            //     child: Container(
-            //       color: Colors.transparent,
-            //     ),
-            //   ),
-            // ),
-            title: Row(
-              children: [
-                UrlImage.circle(
-                  "https://res.cloudinary.com/dnx8aew1t/image/upload/v1732549977/jgldr2myysd7u6vx6sfy.jpg",
-                  size: 42.sp,
+      body: ChatView(
+        chatController: _chatController,
+        onSendTap: _onSendTap,
+        featureActiveConfig: const FeatureActiveConfig(
+          lastSeenAgoBuilderVisibility: true,
+          receiptsBuilderVisibility: true,
+          enableScrollToBottomButton: true,
+        ),
+        scrollToBottomButtonConfig: ScrollToBottomButtonConfig(
+          backgroundColor: theme.textFieldBackgroundColor,
+          border: Border.all(color: Colors.transparent),
+          icon: Icon(
+            Icons.keyboard_arrow_down_rounded,
+            color: context.colorScheme.primary,
+            weight: 10,
+            size: 30,
+          ),
+        ),
+        chatViewState: ChatViewState.hasMessages,
+        chatViewStateConfig: ChatViewStateConfiguration(
+          loadingWidgetConfig: ChatViewStateWidgetConfiguration(
+            loadingIndicatorColor: context.colorScheme.primary,
+          ),
+          onReloadButtonTap: () {},
+        ),
+        typeIndicatorConfig: TypeIndicatorConfiguration(
+          flashingCircleBrightColor: context.colorScheme.primaryFixedDim,
+          flashingCircleDarkColor: context.colorScheme.primaryFixedDim,
+        ),
+        appBar: AppBar(
+          toolbarHeight: 52,
+          leadingWidth: 36,
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          foregroundColor: Theme.of(context).colorScheme.onPrimary,
+          title: Row(
+            children: [
+              UrlImage.circle(
+                  "https://raw.githubusercontent.com/SimformSolutionsPvtLtd/flutter_showcaseview/master/example/assets/simform.png",
+                  size: 36),
+              SizedBox(width: 12),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      widget.title,
+                      style: context.textTheme.labelLarge
+                          ?.copyWith(color: context.colorScheme.onPrimary),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    Text(
+                      "online",
+                      style: context.textTheme.labelSmall
+                          ?.copyWith(color: context.colorScheme.onPrimary),
+                    ),
+                  ],
                 ),
-                Text(
-                  "title",
-                  style: context.textTheme.headlineSmall?.copyWith(
-                      color: context.colorScheme.onSurface,
-                      fontWeight: FontWeight.w600),
-                ),
-              ],
+              ),
+            ],
+          ),
+          actions: [
+            IconButton(
+              onPressed: _onThemeIconTap,
+              icon: Icon(
+                isDarkTheme
+                    ? Icons.brightness_4_outlined
+                    : Icons.dark_mode_outlined,
+                color: context.colorScheme.onPrimary,
+              ),
             ),
-          )
-        ],
-        body: CustomScrollView(
-          physics: const BouncingScrollPhysics(),
-          slivers: [
-            SliverToBoxAdapter(
-              child: SizedBox(
-                height: 100.h,
+            IconButton(
+              tooltip: 'Toggle TypingIndicator',
+              onPressed: _showHideTypingIndicator,
+              icon: Icon(
+                Icons.keyboard,
+                color: context.colorScheme.onPrimary,
+              ),
+            ),
+            IconButton(
+              tooltip: 'Simulate Message receive',
+              onPressed: receiveMessage,
+              icon: Icon(
+                Icons.supervised_user_circle,
+                color: context.colorScheme.onPrimary,
               ),
             ),
           ],
         ),
+        chatBackgroundConfig: ChatBackgroundConfiguration(
+          messageTimeIconColor: Colors.red,
+          messageTimeTextStyle: TextStyle(color: Colors.blue),
+          defaultGroupSeparatorConfig: DefaultGroupSeparatorConfiguration(
+            textStyle: context.textTheme.bodySmall?.copyWith(
+              color: context.colorScheme.onSurfaceVariant,
+            ),
+          ),
+          backgroundColor: context.colorScheme.surface,
+        ),
+        sendMessageConfig: SendMessageConfiguration(
+          imagePickerIconsConfig: ImagePickerIconsConfiguration(
+            cameraIconColor: context.colorScheme.primary,
+            galleryIconColor: context.colorScheme.primary,
+          ),
+          // micIconColor: context.colorScheme.primary,
+          defaultSendButtonColor: context.colorScheme.primary,
+          replyDialogColor: context.colorScheme.primaryContainer,
+          replyTitleColor: context.colorScheme.onPrimaryContainer,
+          replyMessageColor: context.colorScheme.onPrimaryContainer,
+          closeIconColor: context.colorScheme.onSurface,
+          textFieldBackgroundColor: context.colorScheme.surfaceContainerHighest,
+          textFieldConfig: TextFieldConfiguration(
+            onMessageTyping: (status) {
+              /// Do with status
+              logger.d(status.toString());
+            },
+            compositionThresholdTime: const Duration(seconds: 1),
+            textStyle: context.textTheme.bodyLarge
+                ?.copyWith(color: context.colorScheme.onSurface),
+          ),
+          voiceRecordingConfiguration: VoiceRecordingConfiguration(
+            backgroundColor: context.colorScheme.surfaceContainerHighest,
+            recorderIconColor: context.colorScheme.primary,
+            waveStyle: WaveStyle(
+              showMiddleLine: false,
+              waveColor: context.colorScheme.primary,
+              extendWaveform: true,
+            ),
+          ),
+        ),
+        //to do: aaaaaaaaaaaaaaaaaaaa
+        chatBubbleConfig: ChatBubbleConfiguration(
+          outgoingChatBubbleConfig: ChatBubble(
+            linkPreviewConfig: LinkPreviewConfiguration(
+              backgroundColor: theme.linkPreviewOutgoingChatColor,
+              bodyStyle: theme.outgoingChatLinkBodyStyle,
+              titleStyle: theme.outgoingChatLinkTitleStyle,
+            ),
+            receiptsWidgetConfig:
+                const ReceiptsWidgetConfig(showReceiptsIn: ShowReceiptsIn.all),
+            color: theme.outgoingChatBubbleColor,
+          ),
+          inComingChatBubbleConfig: ChatBubble(
+            linkPreviewConfig: LinkPreviewConfiguration(
+              linkStyle: TextStyle(
+                color: theme.inComingChatBubbleTextColor,
+                decoration: TextDecoration.underline,
+              ),
+              backgroundColor: theme.linkPreviewIncomingChatColor,
+              bodyStyle: theme.incomingChatLinkBodyStyle,
+              titleStyle: theme.incomingChatLinkTitleStyle,
+            ),
+            textStyle: TextStyle(color: theme.inComingChatBubbleTextColor),
+            onMessageRead: (message) {
+              /// send your message reciepts to the other client
+              debugPrint('Message Read');
+            },
+            senderNameTextStyle:
+                TextStyle(color: theme.inComingChatBubbleTextColor),
+            color: theme.inComingChatBubbleColor,
+          ),
+        ),
+        replyPopupConfig: ReplyPopupConfiguration(
+          backgroundColor: theme.replyPopupColor,
+          buttonTextStyle: TextStyle(color: theme.replyPopupButtonColor),
+          topBorderColor: theme.replyPopupTopBorderColor,
+        ),
+        reactionPopupConfig: ReactionPopupConfiguration(
+          shadow: BoxShadow(
+            color: isDarkTheme ? Colors.black54 : Colors.grey.shade400,
+            blurRadius: 20,
+          ),
+          backgroundColor: theme.reactionPopupColor,
+        ),
+        messageConfig: MessageConfiguration(
+          messageReactionConfig: MessageReactionConfiguration(
+            backgroundColor: theme.messageReactionBackGroundColor,
+            borderColor: theme.messageReactionBackGroundColor,
+            reactedUserCountTextStyle:
+                TextStyle(color: theme.inComingChatBubbleTextColor),
+            reactionCountTextStyle:
+                TextStyle(color: theme.inComingChatBubbleTextColor),
+            reactionsBottomSheetConfig: ReactionsBottomSheetConfiguration(
+              backgroundColor: theme.backgroundColor,
+              reactedUserTextStyle: TextStyle(
+                color: theme.inComingChatBubbleTextColor,
+              ),
+              reactionWidgetDecoration: BoxDecoration(
+                color: theme.inComingChatBubbleColor,
+                boxShadow: [
+                  BoxShadow(
+                    color: isDarkTheme ? Colors.black12 : Colors.grey.shade200,
+                    offset: const Offset(0, 20),
+                    blurRadius: 40,
+                  )
+                ],
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+          imageMessageConfig: ImageMessageConfiguration(
+            margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 15),
+            shareIconConfig: ShareIconConfiguration(
+              defaultIconBackgroundColor: theme.shareIconBackgroundColor,
+              defaultIconColor: theme.shareIconColor,
+            ),
+          ),
+        ),
+        profileCircleConfig: const ProfileCircleConfiguration(
+          profileImageUrl: Data.profileImage,
+        ),
+        repliedMessageConfig: RepliedMessageConfiguration(
+          backgroundColor: theme.repliedMessageColor,
+          verticalBarColor: theme.verticalBarColor,
+          repliedMsgAutoScrollConfig: RepliedMsgAutoScrollConfig(
+            enableHighlightRepliedMsg: true,
+            highlightColor: Colors.pinkAccent.shade100,
+            highlightScale: 1.1,
+          ),
+          textStyle: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 0.25,
+          ),
+          replyTitleTextStyle: TextStyle(color: theme.repliedTitleTextColor),
+        ),
+        swipeToReplyConfig: SwipeToReplyConfiguration(
+          replyIconColor: theme.swipeToReplyIconColor,
+        ),
+        replySuggestionsConfig: ReplySuggestionsConfig(
+          itemConfig: SuggestionItemConfig(
+            decoration: BoxDecoration(
+              color: theme.textFieldBackgroundColor,
+              borderRadius: BorderRadius.circular(8),
+              border: Border.all(
+                color: theme.outgoingChatBubbleColor ?? Colors.white,
+              ),
+            ),
+            textStyle: TextStyle(
+              color: isDarkTheme ? Colors.white : Colors.black,
+            ),
+          ),
+          onTap: (item) =>
+              _onSendTap(item.text, const ReplyMessage(), MessageType.text),
+        ),
       ),
     );
+  }
+
+  void _onSendTap(
+    String message,
+    ReplyMessage replyMessage,
+    MessageType messageType,
+  ) {
+    _chatController.addMessage(
+      Message(
+        id: DateTime.now().toString(),
+        createdAt: DateTime.now(),
+        message: message,
+        sentBy: _chatController.currentUser.id,
+        replyMessage: replyMessage,
+        messageType: messageType,
+      ),
+    );
+    Future.delayed(const Duration(milliseconds: 300), () {
+      _chatController.initialMessageList.last.setStatus =
+          MessageStatus.undelivered;
+    });
+    Future.delayed(const Duration(seconds: 1), () {
+      _chatController.initialMessageList.last.setStatus = MessageStatus.read;
+    });
+  }
+
+  void _onThemeIconTap() {
+    setState(() {
+      if (isDarkTheme) {
+        theme = LightTheme();
+        isDarkTheme = false;
+      } else {
+        theme = DarkTheme();
+        isDarkTheme = true;
+      }
+    });
   }
 }
